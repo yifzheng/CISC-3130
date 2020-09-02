93eed0d51ca36d78770a50cc3b8bcf3b43206586
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2020-09-02 16:55:26 -0400

    second changes

--- Lab_2/src/Artist.java
+++ Lab_2/src/Artist.java
@@ -1,7 +1,7 @@
 
 public class Artist {
 	String name, song, numOfHits, ranking, url; // variables to store information from csv file
-	
+
 	public Artist() {
 	}
 	public void setName(String str) { // set the string to the variable
@@ -24,27 +24,27 @@ public class Artist {
 		numOfHits = str;
 	}
 	
-	public String getHits() {
+	public String getHits() { // get number of hits
 		return numOfHits;
 	}
 	
-	public void setRanking(String str) {
+	public void setRanking(String str) { // set ranking
 		ranking = str;
 	}
 	
-	public String getRanking() {
+	public String getRanking() { // get song ranking
 		return ranking;
 	}
 	
-	public void setUrl(String str) {
+	public void setUrl(String str) { // set url
 		url = str;
 	}
 	
-	public String getUrl() {
+	public String getUrl() { // get url
 		return url;
 	}
 
-	public String toString() {
-		return getRanking() + "," + getSong() + "," + getName() + "," + getHits() + "," + "," + getUrl();
+	public String toString() { // returns a string of all information based on original csv file
+		return getRanking() + "," + getSong() + "," + getName() + "," + getHits() + "," + getUrl();
 	}
 }
--- Lab_2/src/readFile.java
+++ Lab_2/src/readFile.java
@@ -4,32 +4,41 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+import java.io.*;
 public class readFile {
 	public static void main(String[] args) throws IOException{
 		String fileLocation = "Y:\\Downloads\\regional-global-weekly-latest.csv";
-		BufferedReader rd = new BufferedReader(new FileReader(fileLocation)); // opens file input stream
 		//reads the file line by line
 		String line = "";
-		Scanner sc = new Scanner(rd); // scanner to read the lines from file stream
-		List<Artist> lst = new ArrayList<>(); // make an array list to store data from Artist objects
-		
+		// scanner to read the lines from file stream
+		Scanner sc = new Scanner(new BufferedReader(new FileReader(fileLocation))); 
+		// make an array list to store data from Artist objects
+		List<Artist> lst = new ArrayList<>(); 
+		// each row is an artist object and there are methods to set values to the variables in the artist class
 		while((sc.hasNextLine())) {
-			Artist artist = new Artist();
-			line = sc.nextLine();
-			String[] arr = line.split(",");
-			artist.setRanking(arr[0]);
-			artist.setName(arr[2]);
-			artist.setSong(arr[1]);
-			artist.setHits(arr[3]);
-			artist.setUrl(arr[4]);
-			//index = 0;
-			lst.add(artist);
+			Artist artist = new Artist(); // make the object to store information
+			line = sc.nextLine(); // set the variable equal the next line from file
+			String[] arr = line.split(","); // split the line into parts for storing
+			artist.setRanking(arr[0]); // sets the ranking of the song
+			artist.setSong(arr[1]); // sets the name of the song
+			artist.setName(arr[2]); // sets the name of the artist;
+			artist.setHits(arr[3]); // sets the number of hits in the song
+			artist.setUrl(arr[4]); // set the url
+			lst.add(artist); // adds the artist object to the made list
 		}
 		sc.close(); // close the buffer
 		for (Artist x : lst) {
 			System.out.print(x.toString());
 			System.out.println("");
 		}
+		File file = new File("Ranking.csv"); // creates a new csv file
+		FileWriter csvWriter = new FileWriter(file); // makes a filewriter object to write the data from the list to a new csv file
+		for (Artist x : lst) {
+			//for every artist, use toString method to print out all necessary information
+			csvWriter.write(x.toString() + "\n"); 
+		}
+		csvWriter.flush(); // flush fileWriter
+		csvWriter.close(); // close fileWriter
 	}
+
 }
